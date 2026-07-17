43d576248f2d64c6d8fc25d75f516f062c68d9fa
deanhattenhauer <dean.hattenhauer@outlook.com>
2026-07-16 23:58:16 -0500

    feat: create attacks file with todo list

---
+++ attacks.go
@@ -0,0 +1,6 @@
+// attacks.go
+package main
+
+// TODO: dictionaryAttack - reads rockyou.txt line by line, calls verify on each guess
+// TODO: bruteForceAttack - generates combinations via itertools equivalent, calls verify
+// TODO: cuppAttack - collects target profile, generates candidates, calls verify
